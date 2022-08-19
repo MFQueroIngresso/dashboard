2285d124998435fd31e9565182ea5c5628479095
Daniel Lima <danieldeveloper1@gmail.com>
2022-08-19 09:49:00 -0300

    feat: initial dashboard --fix

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -6,13 +6,13 @@ import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Link from "next/link";
 
 function Copyright(props) {
   return (
@@ -115,14 +115,18 @@ export function Login() {
                 control={<Checkbox value="remember" color="primary" />}
                 label="Lembrar meus dados"
               />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Entrar
-              </Button>
+              <Link href="/dashboard">
+                <a>
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                  >
+                    Entrar
+                  </Button>
+                </a>
+              </Link>
               <Grid container>
                 <Grid item xs>
                   <Link href="#" variant="body2">

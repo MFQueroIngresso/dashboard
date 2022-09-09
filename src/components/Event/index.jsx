cc991b8bebb23c32228b5e56f758cf6b3458febe
Daniel Lima <danieldeveloper1@gmail.com>
2022-09-09 08:11:48 -0300

    new grid

--- src/components/Event/index.jsx
+++ src/components/Event/index.jsx
@@ -18,143 +18,162 @@ export function Event() {
   return (
     <>
       <Card sx={{ maxWidth: "100%", minWidth: 345, mt: 0, mb: 1 }}>
-        <CardHeader
-          avatar={
-            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-              N
-            </Avatar>
-          }
-          action={
-            <IconButton aria-label="settings">
-              <MoreVertIcon />
-            </IconButton>
-          }
-          title="Nando Reis"
-          subheader={
-            <>
-              19 de Agosto, 2022
-              <br />
-              <small>Hillarius VP | São José dos Campos - SP</small>
-            </>
-          }
-        />
+        <Link href="/slug-do-evento">
+          <a>
+            <Grid container>
+              <Grid
+                item
+                xs={3}
+                sx={{ borderRight: "1px solid rgba(0,0,0,.15)" }}
+              >
+                <CardHeader
+                  avatar={
+                    <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                      N
+                    </Avatar>
+                  }
+                  title="Nando Reis"
+                  subheader={
+                    <>
+                      19 de Agosto, 2022
+                      <br />
+                      <small>Hillarius VP | São José dos Campos - SP</small>
+                    </>
+                  }
+                />
+                <CardContent>
+                  <Stack direction="row" spacing={1}>
+                    <Chip
+                      label="Faltam 12 dias para o evento"
+                      size="small"
+                      deleteIcon={<CalendarToday />}
+                      onDelete={() => {}}
+                      sx={{ pr: 1, pl: 1 }}
+                    />
+                  </Stack>
+                </CardContent>
+              </Grid>
 
-        <CardContent>
-          <Stack direction="row" spacing={1}>
-            <Chip
-              label="Faltam 12 dias para o evento"
-              size="small"
-              deleteIcon={<CalendarToday />}
-              onDelete={() => {}}
-              sx={{ pr: 1, pl: 1 }}
-            />
-          </Stack>
-          <br />
-          <Divider />
-          <Grid container spacing={1} sx={{ mt: 1, mb: 2 }}>
-            <Grid item xs={4} sx={{ pr: 1 }}>
-              <Typography
-                variant="body2"
-                color="info.main"
-                sx={{ fontWeight: "bold" }}
+              <Grid
+                item
+                xs={3}
+                sx={{ borderRight: "1px solid rgba(0,0,0,.15)" }}
               >
-                Hoje
-              </Typography>
-            </Grid>
-            <Grid item xs={4}>
-              <Typography variant="body2" color="text.secondary">
-                <b>Vendidos</b>
-                <br />
-                <b>Cortesia</b>
-                <br />
-                <b>Receita</b>
-              </Typography>
-            </Grid>
-            <Grid item xs={4}>
-              <Typography variant="body2" color="success.main">
-                <b>
-                  10
-                  <br />
-                  4
-                  <br />
-                  R$ 12.456,00
-                </b>
-              </Typography>
-            </Grid>
-          </Grid>
-          <Divider />
-          <Grid container spacing={1} sx={{ mt: 1, mb: 2 }}>
-            <Grid item xs={4} sx={{ pr: 1 }}>
-              <Typography
-                variant="body2"
-                color="info.main"
-                sx={{ fontWeight: "bold" }}
+                <CardContent>
+                  <Grid container spacing={1} sx={{ mt: 1, mb: 2 }}>
+                    <Grid item xs={4} sx={{ pr: 1 }}>
+                      <Typography
+                        variant="body2"
+                        color="info.main"
+                        sx={{ fontWeight: "bold" }}
+                      >
+                        Hoje
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={4}>
+                      <Typography variant="body2" color="text.secondary">
+                        <b>Vendidos</b>
+                        <br />
+                        <b>Cortesia</b>
+                        <br />
+                        <b>Receita</b>
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={4}>
+                      <Typography variant="body2" color="success.main">
+                        <b>
+                          10
+                          <br />
+                          4
+                          <br />
+                          R$ 12.456,00
+                        </b>
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </CardContent>
+              </Grid>
+
+              <Grid
+                item
+                xs={3}
+                sx={{ borderRight: "1px solid rgba(0,0,0,.15)" }}
               >
-                Total
-              </Typography>
+                <CardContent>
+                  <Grid container spacing={1} sx={{ mt: 1, mb: 2 }}>
+                    <Grid item xs={4} sx={{ pr: 1 }}>
+                      <Typography
+                        variant="body2"
+                        color="info.main"
+                        sx={{ fontWeight: "bold" }}
+                      >
+                        Total
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={4}>
+                      <Typography variant="body2" color="text.secondary">
+                        <b>Vendidos</b>
+                        <br />
+                        <b>Cortesia</b>
+                        <br />
+                        <b>Receita</b>
+                        <br />
+                        <b>Ticket Médio</b>
+                      </Typography>
+                    </Grid>
+
+                    <Grid item xs={4}>
+                      <Typography variant="body2" color="success.main">
+                        <b>
+                          10
+                          <br />
+                          4
+                          <br />
+                          R$ 12.456,00
+                          <br />
+                          R$ 223,00
+                        </b>
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={4}></Grid>
+                  </Grid>
+                </CardContent>
+              </Grid>
+
+              <Grid item xs={3}>
+                <CardContent>
+                  <Grid container spacing={1} sx={{ mt: 1, mb: 2 }}>
+                    <Grid item xs={4} sx={{ pr: 1 }}>
+                      <Typography
+                        variant="body2"
+                        color="info.main"
+                        sx={{ fontWeight: "bold" }}
+                      >
+                        Média Diária
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={4}>
+                      <Typography variant="body2" color="text.secondary">
+                        <b>Quantidade</b>
+                        <br />
+                        <b>Valor</b>
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={4}>
+                      <Typography variant="body2" color="success.main">
+                        <b>
+                          10
+                          <br />
+                          R$ 233,00
+                        </b>
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </CardContent>
+              </Grid>
             </Grid>
-            <Grid item xs={4}>
-              <Typography variant="body2" color="text.secondary">
-                <b>Vendidos</b>
-                <br />
-                <b>Cortesia</b>
-                <br />
-                <b>Receita</b>
-                <br />
-                <b>Ticket Médio</b>
-              </Typography>
-            </Grid>
-            <Grid item xs={4}>
-              <Typography variant="body2" color="success.main">
-                <b>
-                  10
-                  <br />
-                  4
-                  <br />
-                  R$ 12.456,00
-                  <br />
-                  R$ 223,00
-                </b>
-              </Typography>
-            </Grid>
-          </Grid>
-          <Divider />
-          <Grid container spacing={1} sx={{ mt: 1, mb: 2 }}>
-            <Grid item xs={4} sx={{ pr: 1 }}>
-              <Typography
-                variant="body2"
-                color="info.main"
-                sx={{ fontWeight: "bold" }}
-              >
-                Média Diária
-              </Typography>
-            </Grid>
-            <Grid item xs={4}>
-              <Typography variant="body2" color="text.secondary">
-                <b>Quantidade</b>
-                <br />
-                <b>Valor</b>
-              </Typography>
-            </Grid>
-            <Grid item xs={4}>
-              <Typography variant="body2" color="success.main">
-                <b>
-                  10
-                  <br />
-                  R$ 233,00
-                </b>
-              </Typography>
-            </Grid>
-          </Grid>
-          <Divider />
-        </CardContent>
-        <CardActions sx={{ ml: 1, mb: 2, textAlign: "center" }}>
-          <Button variant="outlined" size="small" sx={{ m: "auto" }}>
-            <Link href="/slug-do-evento">
-              <a>Ver Detalhes</a>
-            </Link>
-          </Button>
-        </CardActions>
+          </a>
+        </Link>
       </Card>
     </>
   );
--- src/components/Events/index.jsx
+++ src/components/Events/index.jsx
@@ -6,31 +6,34 @@ export default function Events() {
   return (
     <div style={{ width: "100%" }}>
       <Grid container spacing={3} sx={{ mt: 2 }}>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
+        <Grid item xs={12}>
           <Event />
         </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
+        <Grid item xs={12}>
           <Event />
         </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
+        <Grid item xs={12}>
           <Event />
         </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
+        <Grid item xs={12}>
           <Event />
         </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
+        <Grid item xs={12}>
           <Event />
         </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
+        <Grid item xs={12}>
           <Event />
         </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
+        <Grid item xs={12}>
           <Event />
         </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
+        <Grid item xs={12}>
           <Event />
         </Grid>
       </Grid>
+      <br />
+      <br />
+      <br />
     </div>
   );
 }

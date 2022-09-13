a16511a99f4d0361d473e7d441fb38e598326a04
Daniel Lima <danieldeveloper1@gmail.com>
2022-09-13 11:26:01 -0300

    loading

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,37 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import Head from "next/head";
+import NextProgress from "next-progress";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="description" content="Lorem Ipsum..." />
+        <title>Example</title>
+      </Head>
+      {
+        //
+        // Can be placed anywhere in the return statement
+        //
+      }
+      <NextProgress
+        delay={300}
+        options={{ showSpinner: true }}
+        color="rgb(5, 30, 52)"
+        height="4px"
+      />
+      {
+        //
+        //
+        //
+      }
+      <>
+        <Component {...pageProps} />
+      </>
+    </>
+  );
 }
-
-export default MyApp

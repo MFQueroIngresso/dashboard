9d1b911a48883c2bebcc9fcaae385c914ec1cc32
Daniel Lima <danieldeveloper1@gmail.com>
2022-11-21 13:27:42 -0300

    adapt query for data without filters

--- pages/api/site_detalhados.js
+++ pages/api/site_detalhados.js
@@ -4,7 +4,14 @@ export default async function handler(req, res) {
   try {
     const result = await executeQueryBdPromo({
       query: `
-        SELECT * FROM tbl_eventos
+        SELECT 
+            eve.eve_cod AS codigo,
+            UPPER(eve.eve_nome) AS evento,
+            eve.eve_data DataEvento,
+            eve.eve_local LocalEvento,
+            eve.eve_cidade CidadeEvento
+        FROM
+            tbl_eventos eve
       `,
     });
 

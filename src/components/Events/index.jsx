1ba7d81fffd9eda2991f86a9f3f6f4f78092c2fa
Daniel Lima <danieldeveloper1@gmail.com>
2022-11-28 11:26:34 -0300

    initial populate events

--- src/components/Events/index.jsx
+++ src/components/Events/index.jsx
@@ -2,13 +2,15 @@ import * as React from "react";
 import Grid from "@mui/material/Grid";
 import { Event } from "../Event";
 
-export default function Events() {
+export default function Events({ events }) {
   return (
     <div style={{ width: "100%" }}>
       <Grid container spacing={1} sx={{ mt: 2 }}>
-        <Grid item xs={12}>
-          <Event />
-        </Grid>
+        {events.map((event) => (
+          <Grid key={event} item xs={12}>
+            <Event />
+          </Grid>
+        ))}
       </Grid>
       <br />
       <br />

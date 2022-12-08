63e4f6b93b581f4b490848b89918610891e7b009
Daniel Lima <danieldeveloper1@gmail.com>
2022-12-07 21:03:55 -0300

    correção na listagem e paginação dos eventos

--- src/components/Events/EventList/index.jsx
+++ src/components/Events/EventList/index.jsx
@@ -9,29 +9,6 @@ import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
 import { Typography } from "@mui/material";
 
-function createData(name, code, population, size) {
-  const density = population / size;
-  return { name, code, population, size, density };
-}
-
-const rows = [
-  createData("India", "IN", 1324171354, 3287263),
-  createData("China", "CN", 1403500365, 9596961),
-  createData("Italy", "IT", 60483973, 301340),
-  createData("United States", "US", 327167434, 9833520),
-  createData("Canada", "CA", 37602103, 9984670),
-  createData("Australia", "AU", 25475400, 7692024),
-  createData("Germany", "DE", 83019200, 357578),
-  createData("Ireland", "IE", 4857000, 70273),
-  createData("Mexico", "MX", 126577691, 1972550),
-  createData("Japan", "JP", 126317000, 377973),
-  createData("France", "FR", 67022000, 640679),
-  createData("United Kingdom", "GB", 67545757, 242495),
-  createData("Russia", "RU", 146793744, 17098246),
-  createData("Nigeria", "NG", 200962417, 923768),
-  createData("Brazil", "BR", 210147125, 8515767),
-];
-
 export function EventList({ events }) {
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
@@ -99,74 +76,76 @@ export function EventList({ events }) {
             </TableRow>
           </TableHead>
           <TableBody>
-            {events.map((event) => {
-              return (
-                <>
-                  <TableRow>
-                    <TableCell>
-                      <div>
-                        <strong>{event.eve_nome}</strong>
-                      </div>
-                      <small>{event.eve_local}</small>
-                      <span>&nbsp; | &nbsp;</span>
-                      <small>{event.eve_cidade}</small>
-                    </TableCell>
-                    <TableCell>
-                      <span>
+            {events
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((event) => {
+                return (
+                  <>
+                    <TableRow key={event}>
+                      <TableCell>
+                        <div>
+                          <strong>{event.eve_nome}</strong>
+                        </div>
+                        <small>{event.eve_local}</small>
+                        <span>&nbsp; | &nbsp;</span>
+                        <small>{event.eve_cidade}</small>
+                      </TableCell>
+                      <TableCell>
+                        <span>
+                          <strong>
+                            {new Date(event.eve_data).toLocaleString("pt-BR", {
+                              year: "numeric",
+                              month: "numeric",
+                              day: "numeric",
+                            })}
+                          </strong>
+                        </span>
+                        <br />
+                        <Typography
+                          variant="strong"
+                          component="strong"
+                          color={
+                            showRemaingDays(event) < 0
+                              ? "secondary.main"
+                              : "primary.main"
+                          }
+                        >
+                          {showRemaingDays(event) < 0
+                            ? "Evento Encerrado"
+                            : `Faltam ${showRemaingDays(
+                                event
+                              )} dias para o evento`}
+                        </Typography>
+                      </TableCell>
+                      <TableCell>8</TableCell>
+                      <TableCell>
                         <strong>
-                          {new Date(event.eve_data).toLocaleString("pt-BR", {
-                            year: "numeric",
-                            month: "numeric",
-                            day: "numeric",
-                          })}
+                          <Typography
+                            variant="span"
+                            component="span"
+                            color="success.main"
+                          >
+                            R$ 480,00
+                          </Typography>
                         </strong>
-                      </span>
-                      <br />
-                      <Typography
-                        variant="strong"
-                        component="strong"
-                        color={
-                          showRemaingDays(event) < 0
-                            ? "secondary.main"
-                            : "primary.main"
-                        }
-                      >
-                        {showRemaingDays(event) < 0
-                          ? "Evento Encerrado"
-                          : `Faltam ${showRemaingDays(
-                              event
-                            )} dias para o evento`}
-                      </Typography>
-                    </TableCell>
-                    <TableCell>8</TableCell>
-                    <TableCell>
-                      <strong>
-                        <Typography
-                          variant="span"
-                          component="span"
-                          color="success.main"
-                        >
-                          R$ 480,00
-                        </Typography>
-                      </strong>
-                    </TableCell>
-                    <TableCell>0</TableCell>
-                    <TableCell>243 </TableCell>
-                    <TableCell>
-                      <strong>
-                        <Typography
-                          variant="span"
-                          component="span"
-                          color="success.main"
-                        >
-                          R$ 17.060,00
-                        </Typography>
-                      </strong>
-                    </TableCell>
-                  </TableRow>
-                </>
-              );
-            })}
+                      </TableCell>
+                      <TableCell>0</TableCell>
+                      <TableCell>243 </TableCell>
+                      <TableCell>
+                        <strong>
+                          <Typography
+                            variant="span"
+                            component="span"
+                            color="success.main"
+                          >
+                            R$ 17.060,00
+                          </Typography>
+                        </strong>
+                      </TableCell>
+                    </TableRow>
+                  </>
+                );
+              })}
           </TableBody>
         </Table>
       </TableContainer>
